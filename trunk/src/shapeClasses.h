a00630feaaac97a11709daea18ce86993b7e4b2e
farooq <farooq@3fcdfe01-befb-4de0-9aaa-81d4169d85b9>
2006-10-19 22:16:42 +0000

    added multishape object for grouping

--- trunk/src/shapeClasses.h
+++ trunk/src/shapeClasses.h
@@ -7,129 +7,33 @@ Created by Farooq Ahmad Sept. 2006
 int lastX; int lastY; 
 
 #include "object.h"
+#include "sphere.h"
+#include "cube.h"
+#include "rectangle.h"
 
-
-
-/*
 class multiShape:public object{
-	multishape(vector<object *> shapePtrs, float _x, float _y,  float _z, float _r1, float _rY, float _r3, float _sx, float _sy, float _sz){};
-	void	draw(){
+public:
+	multiShape(){};
+	multiShape(std::vector<object *> _shapePtrs
+	, float _x, float _y,  float _z, float _rX, float _rY, float _rZ, float _sX, float _sY, float _sZ){
+		xOff = _x; yOff = _y;  zOff = _z; 
+		rX = _rX; rY = _rY; rZ = _rZ;
+		sX = _sX; sY = _sY; sZ = _sZ;
+		//shapePtrs = _shapePtrs;
+		//copy(_shapePtrs.begin(), _shapePtrs.end(), shapePtrs.begin());
+		copy(_shapePtrs.begin(), _shapePtrs.end(), std::back_insert_iterator<std::vector<object *> >(shapePtrs));
+	};
 
+object* clone()   { return new multiShape(*this); }
 
-	}
-
-
-
+std::vector<object *> ungroup(){
+	
+ for (std::vector<object *>::iterator it = shapePtrs.begin(); it!=shapePtrs.end();) {
+	 (*it)->transform(xOff, yOff, zOff, rX, rY, rZ, sX, sY, sZ);
+	 ++it;
+ }
+	return shapePtrs;
 }
-*/
-
-class sphere:public object{
-public:
-	sphere(){ 
-	
-	};
-	sphere(int _name, float _x, float _y,  float _z, float _rad)//:
-		//object(_name, _x1, _y1, _x2, _y2, _r)
-	{
-		name = _name; xOff = _x; yOff = _y;  zOff = _z;sX = 1; sY = 1; sZ =1; radius = _rad; isVisible = 1; rY = 0; rX = 0;
-	}
-	sphere(int _name, float _x, float _y,  float _z, float _rX, float _rY, float _rZ, 
-		float _sX, float _sY, float _sZ, float _rad)//:
-		//object(_name, _x1, _y1, _x2, _y2, _r)
-	{
-		name = _name; xOff = _x; yOff = _y;  zOff = _z;
-		rX = _rX; rY = _rY; rZ = _rZ;
-		sX = _sX; sY = _sY; sZ = _sZ; 
-		radius = _rad; isVisible = 1; 
-	}
-
-	
-	object* clone()  { return new sphere(*this); }
-
-	std::string getDataString(){
-		std::ostringstream data;
-		data<<"SPHERE "<<xOff<<" "<<yOff<<" "<<zOff<<" "<<rX<<" "<<rY<<" "<<rZ<<" "<<sX<<" "<<sY<<" "<<sZ<<" "<<radius;	
-		return data.str(); 
-	}
-	virtual std::string getSLDataString(){
-		std::ostringstream data;
-		data<<"<type val=\"3\"> "<<std::endl;
-		data<<"<position x=\""<<xOff/100<<"\" y=\""<<yOff/100<<"\" z=\""<<zOff/100<<"\">"<<std::endl;
-		data<<"<rotation x=\""<<PI/180*(rX-90)<<"\" y=\""<<PI/180*rY<<"\" z=\""<<PI/180*rZ<<"\">"<<std::endl;
-		data<<"<size x=\""<<radius*sX/100<<"\" y=\""<<radius*sY/100<<"\" z=\""<<radius*sZ/100<<"\">"<<std::endl;
-		return data.str(); 
-	}
-
-
-
-	void	draw(){
-	if (isVisible == 1){
-	if (isSelected == 1){
-		highlight();
-	}
-	//glPushName(name);
-	glPushMatrix();		
-		glTranslatef(xOff,yOff,zOff);
-		glScalef(sX, sY, sZ);
-		glRotatef(rX,0,1,0);
-		glRotatef(rY,1,0,0);
-
-
-	startLighting(mat_ambient);
-
-		glutSolidSphere(radius, 10, 10);
-		glDisable ( GL_LIGHTING ) ;
-
-	glPopMatrix();
-	//glPopName();
-	}
-	}
-	float radius;
-};
-
-
-
-
-class cube:public object{
-public:
-	cube(){ 
-	
-	};
-	cube(int _name, float _x, float _y,  float _z, float _size)//:
-		//object(_name, _x1, _y1, _x2, _y2, _r)
-	{
-		name = _name; xOff = _x; yOff = _y;  zOff = _z; sX = 5;sY = 2; sZ= 0.2; size = _size; isVisible = 1; rX = 0; rY = 0;
-	}
-	cube(int _name, float _x, float _y,  float _z, 
-		float _rX, float _rY, float _rZ, float _sX, float _sY, float _sZ,
-		float _size)//:
-		//object(_name, _x1, _y1, _x2, _y2, _r)
-	{
-		name = _name; xOff = _x; yOff = _y;  zOff = _z; 
-		rX = _rX; rY = _rY; rZ = _rZ;
-		sX = _sX; sY = _sY; sZ = _sZ; 
-		size = _size; isVisible = 1; 
-	}
-
-
-object* clone()   { return new cube(*this); }
-
-
-std::string getDataString(){
-		std::ostringstream data;
-		data<<"CUBE "<<xOff<<" "<<yOff<<" "<<zOff<<" "<<rX<<" "<<rY<<" "<<rZ<<" "<<sX<<" "<<sY<<" "<<sZ<<" "<<size;	
-		return data.str(); 
-	}
-
-	virtual std::string getSLDataString(){
-		std::ostringstream data;
-		data<<"<type val=\"0\"> "<<std::endl;
-		data<<"<position x=\""<<xOff/100<<"\" y=\""<<yOff/100<<"\" z=\""<<zOff/100<<"\">"<<std::endl;
-		data<<"<rotation x=\""<<PI/180*(rX-90)<<"\" y=\""<<PI/180*rY<<"\" z=\""<<PI/180*rZ<<"\" s =\"1.0\""<<">"<<std::endl;
-		data<<"<size x=\""<<size*sX/100<<"\" y=\""<<size*sY/100<<"\" z=\""<<size*sZ/100<<"\">"<<std::endl;
-		return data.str(); 
-	}
-
 
 virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 		
@@ -168,177 +72,48 @@ virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 
 
 		if (but == GLUT_LEFT_BUTTON){
-		xOff += xNew; zOff += yNew;
+		
+			xOff += xNew; zOff += yNew;
+
+
+
 		}
 		else if (but == GLUT_MIDDLE_BUTTON){
 		 yOff -= y;
 		}
-		else if (but == GLUT_MIDDLE_BUTTON){
-		 rX += x; rY+= y;//sOff += y;
-		}
 		}
 
 	}
 
-void drawRect(){
 
-	
-	glBegin( GL_POLYGON );
-//first face   
-//drawSquare(size);
-glVertex3f( -size/2, -size/2, 0.0f );
-   glTexCoord2f( 0.0f, 0.0f );
-   glVertex3f(  size/2, -size/2, 0.0f );
-   glTexCoord2f( 1.0f, 0.0f );
-   glVertex3f(  size/2,  size/2, 0.0f );
-   glTexCoord2f( 1.0f, 1.0f );
-   glVertex3f( -size/2,  size/2, 0.0f );
-   glTexCoord2f( 0.0f, 1.0f );
-  glEnd();
-
-}
-
-void highlightCorners(){
-glPushMatrix();
-glTranslatef(xOff,yOff,zOff);
-		glRotatef(rX,0,1,0);
-		glRotatef(rY,1,0,0);
-		glScalef(sX, sY, sZ);
-
-		glDisable(GL_LIGHTING);
-		glColor3f(0.85, 0.1, 0.1);
-glPushMatrix();
-glTranslatef(-size/2, -size/2, -size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(-size/2, -size/2, size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
+	virtual void	draw(){
 
 
-glPushMatrix();
-glTranslatef(-size/2, size/2, -size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(-size/2, size/2, size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(size/2, -size/2, -size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(size/2, -size/2, size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(size/2, size/2, -size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-glPushMatrix();
-glTranslatef(size/2, size/2, size/2);
-glScalef(1/sX, 1/sY, 1/sZ);
-glutSolidCube(5);
-glPopMatrix();
-
-
-
-
-
-glPopMatrix();
-}
-
-
-
-	void	draw(){
-	if (isVisible == 1){
-	if (isSelected == 1){
-		highlight();
-		highlightCorners();
-	}
-	//glPushName(name);
-	glPushMatrix();		
+		glPushMatrix();		
 		glTranslatef(xOff,yOff,zOff);
 		glRotatef(rX,0,1,0);
 		glRotatef(rY,1,0,0);
 		glScalef(sX, sY, sZ);
-startLighting(mat_ambient);
-
-if (texture != 0) {
-	//std::cout<<"enabling texture "<<std::endl;	
-	glEnable( GL_TEXTURE_2D );	
-	glBindTexture( GL_TEXTURE_2D, texture);
-
-}
 
 
-//glColor3f(0.85, 0.1, 0.1);
-//first face
-glPushMatrix();
-glTranslatef(0.0f, 0.0, size/2);
-drawRect();
-glPopMatrix();
-//second face
-glPushMatrix();
-glRotatef(90,1,0,0);
-glTranslatef(0, 0, -size/2);
-drawRect();
-glPopMatrix();
- //thirdface
-glPushMatrix();
-glTranslatef(0.0f, 0.0, -size/2);
-drawRect();
-glPopMatrix();
-   //fourth face
-glPushMatrix();
-glRotatef(90,1,0,0);
-glTranslatef(0, 0, size/2);
-drawRect();
-glPopMatrix();
-//fifth face
-   glPushMatrix();
-glRotatef(90,0,1,0);
-glTranslatef(0, 0, size/2);
-drawRect();
-glPopMatrix();
- //sixth face
-   glPushMatrix();
-glRotatef(90,0,1,0);
-glTranslatef(0, 0, -size/2);
-drawRect();
-glPopMatrix();
+		for (int i = 0; i < shapePtrs.size(); i++){
+				shapePtrs[i]->drawTopLevel();			
+		}
 
-
-
-///glColor3f(0.85, 0.1, 0.1);
-///		glutSolidCube(size);
-	glDisable ( GL_LIGHTING ) ;
-
-	glPopMatrix();
-
-glDisable( GL_TEXTURE_2D );	
-
-
-	//glPopName();
+		glPopMatrix();
 	}
-	}
-	float size;
+
+
+	
+
+	std::vector<object *> shapePtrs;
+	
+	
 };
+
+
+
+
 
 
 
@@ -627,334 +402,6 @@ glColor3f(0.85, 0.1, 0.1);
 };
 
 
-
-class rectangle:public object{
-public:
-	rectangle(){ 
-	
-	};
-	rectangle(int _name, float _x1, float _y1,  float _x2,float _y2,float _r)//:
-		//object(_name, _x1, _y1, _x2, _y2, _r)
-	{
-		name = _name; x1 = _x1; y1 = _y1;  x2 = _x2; y2 = _y2; rX = 0; sX = 1; sY = 1; sZ= 1; isVisible = 1; rY = _r;
-		xOff = 0; yOff = 0; zOff = 0;
-	}
-	rectangle(int _name, float _xOff, float _yOff, float _zOff, 
-		float _rX, float _rY, float _rZ, float _sX, float _sY, float _sZ,
-		float _x1, float _y1,  float _x2,float _y2)//:
-		//object(_xOff, _yOff,_zOff, _rX, _rY, _rZ, _sX, _sY, _sZ)
-	{
-		name = _name; x1 = _x1; y1 = _y1;  x2 = _x2; y2 = _y2; 
-		rX = _rX; rY = _rY; rZ = _rZ;
-		sX = _sX; sY = _sY; sZ= _sZ; isVisible = 1; 
-		xOff = _xOff; yOff = _yOff; zOff = _zOff;
-	}
-
-
-
-
-	rectangle* clone()   { std::cout<<"rect clone"<<std::endl; return new rectangle(*this); }
-
-std::string getDataString(){
-		std::ostringstream data;
-		data<<"RECTANGLE "<<xOff<<" "<<yOff<<" "<<zOff<<" "<<rX<<" "<<rY<<" "<<rZ<<" "<<sX<<" "<<sY<<" "<<sZ<<" "<<x1<<" "<<y1<<" "<<x2<<" "<<y2;	
-		return data.str(); 
-	}
-
-	virtual std::string getSLDataString(){
-		std::ostringstream data;
-		data<<"<type val=\"0\"> "<<std::endl;
-		data<<"<position x=\""<<xOff/100<<"\" y=\""<<yOff/100<<"\" z=\""<<zOff/100<<"\">"<<std::endl;
-		data<<"<rotation x=\""<<PI/180*(rX-90)<<"\" y=\""<<PI/180*rY<<"\" z=\""<<PI/180*rZ<<"\">"<<std::endl;
-		data<<"<size x=\""<<(x2-x1)*sX/100<<"\" y=\""<<(y2-y1)*sY/100<<"\" z=\""<<0.0001<<"\">"<<std::endl;
-		return data.str(); 
-	}
-
-void initSelection(int but, int key, int x, int y){
-
-	GLint vPort[4];
-	glGetIntegerv( GL_VIEWPORT, vPort );
-	GLfloat mouseX = (float) lastX;
-	GLfloat mouseY = (float)vPort[3] - (float) lastY;
-
-	d1 = distance ( (float) mouseX, (float) mouseY, (float) winX1, (float) winY1);
-	min = d1;
-
-	d2 = distance ( (float) mouseX, (float) mouseY, (float) winX2, (float) winY2);
-	if (d2 < min ) min = d2;
-	 d3 = distance ( (float) mouseX, (float) mouseY, (float) winX3, (float) winY3);
-	if (d3 < min ) min = d3;
-	d4 = distance ( (float) mouseX, (float) mouseY, (float) winX4, (float) winY4);
-	if (d4 < min ) min = d4;
-
-	std::cout<<" Point 1 projected to "<<winX1<<" "<<winY1<<" "<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d1<<std::endl;
-	std::cout<<" Point 2 projected to "<<winX2<<" "<<winY2<<" "<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d2<<std::endl;
-	std::cout<<" Point 3 projected to "<<winX3<<" "<<winY3<<" "<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d3<<std::endl;
-	std::cout<<" Point 4 projected to "<<winX4<<" "<<winY4<<" "<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d4<<std::endl;
-
-	std::cout<<" closest is "<<min<<std::endl;
-
-
-}
-
-float distance(float x1, float y1, float x2, float y2){
-	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-}
-
-
-void setWinCoords(){
-	GLint vPort[4];
-	GLdouble mMatrix[16];
-	GLdouble pMatrix[16];
-	//GLdouble winX, winY, winZ;
-	GLdouble  winZ;
-
-
-
-
-	glGetDoublev( GL_MODELVIEW_MATRIX, mMatrix);
-	glGetDoublev( GL_PROJECTION_MATRIX, pMatrix);
-	glGetIntegerv( GL_VIEWPORT, vPort );
-	//std::cout<<"Closest vertex is "<<
-
-	GLfloat mouseX = (float) lastX;
-	GLfloat mouseY = (float)vPort[3] - (float) lastY;
-
-
-	gluProject(x1,y1,0, mMatrix, pMatrix, vPort,&winX1, &winY1, &winZ);
-	gluProject(x1,y2,0, mMatrix, pMatrix, vPort,&winX2, &winY2, &winZ);
-	gluProject(x2,y2,0, mMatrix, pMatrix, vPort,&winX3, &winY3, &winZ);
-	gluProject(x2,y1,0, mMatrix, pMatrix, vPort,&winX4, &winY4, &winZ);
-
-/*	
-
-	d1 = distance ( (float) mouseX, (float) mouseY, (float) winX1, (float) winY1);
-	min = d1;
-
-	d2 = distance ( (float) mouseX, (float) mouseY, (float) winX2, (float) winY2);
-	if (d2 < min ) min = d2;
-	 d3 = distance ( (float) mouseX, (float) mouseY, (float) winX3, (float) winY3);
-	if (d3 < min ) min = d3;
-	d4 = distance ( (float) mouseX, (float) mouseY, (float) winX4, (float) winY4);
-	if (d4 < min ) min = d4;
-
-	std::cout<<" Point 1 projected to "<<winX1<<" "<<winY1<<" "<<winZ<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d1<<std::endl;
-	std::cout<<" Point 2 projected to "<<winX2<<" "<<winY2<<" "<<winZ<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d2<<std::endl;
-	std::cout<<" Point 3 projected to "<<winX3<<" "<<winY3<<" "<<winZ<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d3<<std::endl;
-	std::cout<<" Point 4 projected to "<<winX4<<" "<<winY4<<" "<<winZ<<std::endl;
-	std::cout<<" dist from "<<lastX<<" "<<lastY<<": "<<d4<<std::endl;
-
-	std::cout<<" closest is "<<min<<std::endl;
-
-*/
-
-}
-
-
-	virtual void move(double patt_trans[3][4], int but, int key, int x, int y){
-		//int specialKey = glutGetModifiers();
-		double xNew, yNew;
-
-		double xGrow, yGrow;
-			getTransformedMotion(patt_trans, but, key, x, y, xNew, yNew);
-
-			std::cout<<"rect rz: "<<rZ<<std::endl;
-		getTransformedMotion(patt_trans, but, key, x, y,rX, xGrow, yGrow);
-
-			//if (key ==(GLUT_ACTIVE_CTRL | GLUT_ACTIVE_ALT)){
-		if (key == GLUT_ACTIVE_ALT){
-		if (but == GLUT_LEFT_BUTTON){
-		sX += (float)x / 25; sZ -= (float)y / 25; //yOff += y;
-		}
-		else if (but == GLUT_RIGHT_BUTTON){
-		 sY -= (float)y / 25;
-		}
-		else if (but == GLUT_MIDDLE_BUTTON){
-		 //sX -= (float)y / 25; sZ -= (float)y / 25;
-			sY -= (float)y / 25;
-		}
-		}
-	 else if (key == GLUT_ACTIVE_CTRL){
-		if (but == GLUT_LEFT_BUTTON){
-		rX += x; 
-		////rY+=y; //yOff += y;
-		}
-		else if (but == GLUT_RIGHT_BUTTON){
-		rY += x;
-		}
-		else if (but == GLUT_MIDDLE_BUTTON){
-		rX+=x;
-		}
-		}
-		//	}
-
-		else{
-		if (but == GLUT_LEFT_BUTTON){
-		
-			if (min > 10){
-			xOff += xNew; zOff += yNew;
-			}
-
-			else{
-
-		if (min == d1){x1 += xGrow; y1+= yGrow;}
-		else if (min == d2){x1+=xGrow/2; y2+=yGrow/2; }
-		else if (min == d3){x2+=xGrow/2; y2+=yGrow/2; }
-		else if (min == d4){x2+=xGrow/2; y1+=yGrow/2; }
-			}
-
-
-
-		}
-		else if (but == GLUT_MIDDLE_BUTTON){
-		 yOff -= y;
-		}
-		else if (but == GLUT_MIDDLE_BUTTON){
-		 rX += x; rY+= y;//sOff += y;
-		}
-		}
-
-	}
-
-
-	void highlightCorners(){
-glPushMatrix();		
-	glTranslatef(xOff, yOff, zOff);
-	glRotatef(rX,0,1,0);
-	glRotatef(rY,1,0,0);
-	glScalef(sX, sZ, 1);
-	//setWinCoords();
-glDisable(GL_LIGHTING);
-glColor3f(0.85, 0.1, 0.1);
-glPushMatrix();
-glTranslatef(x1,y1, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x1,y2, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x2,y1, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x2,y2, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPopMatrix();
-};
-
-
-	void	draw(){
-	if (isVisible == 1){
-	if (isSelected == 1){
-		highlight();
-		highlightCorners();
-	}
-	//glPushName(name);
-	glPushMatrix();		
-
-	glTranslatef(xOff, yOff, zOff);
-		//glTranslatef(xOff - (x2 - x1)/ 2,yOff - (y2 - y1)/2,zOff);
-		//move to center
-		//glTranslatef((x2 - x1)/ 2,(y2 - y1)/2,0);
-
-
-		glRotatef(rX,0,1,0);
-		glRotatef(rY,1,0,0);
-
-
-
-
-
-		glScalef(sX, sZ, 1);
-
-
-//initSelection();
-		setWinCoords();
-
-/*
-glDisable(GL_LIGHTING);
-glColor3f(0.85, 0.1, 0.1);
-glPushMatrix();
-glTranslatef(x1,y1, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x1,y2, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x2,y1, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-glPushMatrix();
-glTranslatef(x2,y2, 0.0f);
-glScalef(1/sX,1/sZ,1);
-glutSolidCube(5);
-glPopMatrix();
-*/
-
-
-		startLighting(mat_ambient);
-
-if (texture != 0) {
-	//std::cout<<"enabling texture "<<std::endl;	
-	glEnable( GL_TEXTURE_2D );	
-	glBindTexture( GL_TEXTURE_2D, texture);
-
-}
-
-glBegin( GL_POLYGON );
-//first face   
-//drawSquare(size);
-
-
-
-glVertex3f( x1, y1, 0.0f );
-   glTexCoord2f( 0.0f, 0.0f );
-   glVertex3f(  x1, y2, 0.0f );
-   glTexCoord2f( 1.0f, 0.0f );
-   glVertex3f(  x2,  y2, 0.0f );
-   glTexCoord2f( 1.0f, 1.0f );
-   glVertex3f( x2,  y1, 0.0f );
-   glTexCoord2f( 0.0f, 1.0f );
-  glEnd();
-
-
-		//glRectf(x1,y1,x2,y2);
-
-
-
-	glDisable ( GL_LIGHTING ) ;
-	glPopMatrix();
-	//glPopName();
-glDisable(GL_TEXTURE_2D);
-
-	}
-	}
-	float x1, y1,  x2, y2;
-
-		float d1, d2, d3, d4, min;
-	GLdouble winX1, winY1, winX2, winY2, winX3, winY3, winX4, winY4;
-
-
-};
 
 class triangle:public object{
 public:
