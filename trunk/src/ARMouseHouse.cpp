7b46ecd4c257d8926593dcb60942ea967862fc4b
nathan <nathan@3fcdfe01-befb-4de0-9aaa-81d4169d85b9>
2006-10-22 17:00:07 +0000

    moved some code from ARMouseHouse.cpp into ARMouseHouse.h

--- trunk/src/ARMouseHouse.cpp
+++ trunk/src/ARMouseHouse.cpp
@@ -26,47 +26,7 @@ Created by Farooq Ahmad Sept. 2006
 #include "myModel.h"
 #include "world.h"
 #include "MainWindow.h" // uses System::... causes ambiguities for myModel calling MessageBox
-
-
-//ARToolkit stuff
-#ifdef _WIN32
-char		*vconf = "Data\\WDM_camera_flipV.xml";
-#else
-char		*vconf = "";
-#endif
-
-#define VIEW_SCALEFACTOR		1.0			// 1.0 ARToolKit unit becomes 0.025 of my OpenGL units.
-#define VIEW_DISTANCE_MIN		0.1			// Objects closer to the camera than this will not be displayed.
-#define VIEW_DISTANCE_MAX		10000.0		// Objects further away from the camera than this will not be displayed.
-
-int			xsize, ysize;
-int			thresh = 100;
-int			ar_count = 0;
-
-static ARUint8					*gARTImage		= NULL; // current image
-char							*gCparam_name	= "Data/camera_para.dat";
-ARParam							 gCparam;				// camera parameters
-static ARGL_CONTEXT_SETTINGS_REF gArglSettings	= NULL;
-
-char	*gPatt_name		= "Data/patt.triangle";
-double	gPatt_width		= 80.0;
-double	gPatt_center[2]	= {0.0, 0.0};
-double	gPatt_trans[3][4];
-int		gPatt_id;
-int		gPatt_found		= false;
-
-int		lastButton;
-int		specialKey;
-
-/*
-#pragma comment( lib, "opengl32.lib" )	// Search For OpenGL32.lib While Linking ( NEW )
-#pragma comment( lib, "glu32.lib" )		// Search For GLu32.lib While Linking    ( NEW )
-#pragma comment( lib, "glaux.lib" )		// Search For GLaux.lib While Linking    ( NEW )
-*/
-using namespace std;
-World w1("myworld.txt");
-//World w1;
-
+#include "ARMouseHouse.h"
 
 AUX_RGBImageRec *LoadBMP(const char *Filename)						// Loads A Bitmap Image
 {
