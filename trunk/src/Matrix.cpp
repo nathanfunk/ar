eff35d07000df4e28bd1d49d5e34b9cf132ce05f
farooq <farooq@3fcdfe01-befb-4de0-9aaa-81d4169d85b9>
2006-11-14 20:05:04 +0000

    added functions for rotation about arbitrary point/axis

--- trunk/src/Matrix.cpp
+++ trunk/src/Matrix.cpp
@@ -17,7 +17,13 @@
 #include "Timer.h"
 #include "Matrix.h"
 
+
+#pragma unmanaged
+
+
+
 using namespace PL3D;
+
 
 Matrix::Matrix()
 {
@@ -63,6 +69,14 @@ void Matrix::setTranslation( const float *translation )
 	m_matrix[14] = translation[2];
 }
 
+void Matrix::translate( float x, float y, float z )
+{
+	m_matrix[12] += x;
+	m_matrix[13] += y;
+	m_matrix[14] += z;
+}
+
+
 void Matrix::setInverseTranslation( const float *translation )
 {
 	m_matrix[12] = -translation[0];
@@ -79,6 +93,53 @@ void Matrix::setRotationDegrees( const float *angles )
 	setRotationRadians( vec );
 }
 
+void Matrix::setRotationDegrees( float rX, float rY, float rZ)
+{
+	float vec[3];
+	vec[0] = ( float )( rX*PI/180.0 );
+	vec[1] = ( float )( rY*PI/180.0 );
+	vec[2] = ( float )( rZ*PI/180.0 );
+	setRotationRadians( vec );
+}
+
+
+void Matrix::setScale(float sX, float sY, float sZ){
+	m_matrix[0] = sX;
+	m_matrix[5] =  sY;
+	m_matrix[10] =  sZ;
+	m_matrix[15] =  1;
+}
+
+
+void Matrix::setRotationDegrees(float theta, float x, float y, float z)
+{
+//theta in degrees: convert to radians;
+	float radians = ( float )( theta*PI/180.0 );
+//normalize the vector
+	float vMag = sqrt(x*x + y*y + z*z);
+	if (vMag == 0 ) return;
+
+	x = x/vMag; y = y/vMag; z = z/vMag;
+	setRotationRadians(radians, x, y, z);
+}
+
+
+
+
+void Matrix::setRotationDegrees(float theta, float a, float b, float c,
+								float u, float v, float w)
+{
+//theta in degrees: convert to radians;
+	float radians = ( float )( theta*PI/180.0 );
+//normalize the vector
+	float vMag = sqrt(u*u + v*v + w*w);
+	if (vMag == 0 ) return;
+
+	u = u/vMag; v = v/vMag; w = w/vMag;
+	setRotationRadians(radians, a,b,c, u, v, w);
+}
+
+
 void Matrix::setInverseRotationDegrees( const float *angles )
 {
 	float vec[3];
@@ -86,6 +147,99 @@ void Matrix::setInverseRotationDegrees( const float *angles )
 	vec[1] = ( float )( angles[1]*PI/180.0 );
 	vec[2] = ( float )( angles[2]*PI/180.0 );
 	setInverseRotationRadians( vec );
+}
+
+
+//rotation about a line through point (a,b,c) parallel to <u,v,w>
+void Matrix::setRotationRadians(float radians,  float a, float b, float c,
+								float u, float v, float w)
+{
+
+	float cosine = cos(radians);
+    float sine = sin(radians);
+    float one_minus_cosine = 1 - cosine;
+	float L2 = u*u + v*v + w*w;
+	float L = sqrt(L2);
+	float u2 = u*u;
+	float v2 = v*v;
+	float w2 = w*w;
+
+
+	m_matrix[0] =  u2 + (v2+w2)*cosine;
+	m_matrix[1] =  u*v*one_minus_cosine + w*L*sine;
+	m_matrix[2] =  u*w*one_minus_cosine - v*L*sine;
+	m_matrix[3] =  0;
+
+	m_matrix[4] =  u*v*one_minus_cosine - w*L*sine;
+	m_matrix[5] =  v2 +(u2+w2)*cosine;
+	m_matrix[6] =  v*w*one_minus_cosine + u*L*sine;
+	m_matrix[7] =  0;
+
+	m_matrix[8] =  u*w*one_minus_cosine + v*L*sine;
+	m_matrix[9] =  v*w*one_minus_cosine - u*L*sine;
+	m_matrix[10] = w2 +(u2+v2)*cosine;
+	m_matrix[11] = 0;
+
+	m_matrix[12] = a*(v2+w2) - u*(b*v + c*w) + (u*(b*v+c*w)-a*(v2+w2))*cosine + (b*w - c*v)*L*sine;
+	m_matrix[13] = b*(u2+w2) - v*(a*u + c*w) + (v*(a*u+c*w)-b*(u2+w2))*cosine + (c*u - a*w)*L*sine;
+	m_matrix[14] = c*(u2+v2) - w*(a*u + b*v) + (w*(a*u+b*v)-c*(u2+v2))*cosine + (a*v - b*u)*L*sine;
+	m_matrix[15] = 1;
+
+}
+
+void Matrix::setRotationRadians(float radians,  float x, float y, float z)
+{
+
+
+float cosine = cos(radians);
+    float sine = sin(radians);
+    float one_minus_cosine = 1 - cosine;
+
+
+/*
+M.x[0][0] = x * x + (1.0 - x * x) * cosine;
+    M.x[0][1] = x * y * one_minus_cosine + z * sine;
+    M.x[0][2] = x * z * one_minus_cosine - y * sine;
+    M.x[0][3] = 0;
+
+    M.x[1][0] = x * y * one_minus_cosine - z * sine;
+    M.x[1][1] = y * y + (1.0 - y * y) * cosine;
+    M.x[1][2] = y * z * one_minus_cosine + x * sine;
+    M.x[1][3] = 0;
+
+    M.x[2][0] = x * z * one_minus_cosine + y * sine;
+    M.x[2][1] = y * z * one_minus_cosine - x * sine;
+    M.x[2][2] = z * z + (1.0 - z * z) * cosine;
+    M.x[2][3] = 0;
+
+    M.x[3][0] = 0;
+    M.x[3][1] = 0;
+    M.x[3][2] = 0;
+    M.x[3][3] = 1;
+*/
+
+	m_matrix[0] =  x * x *(1- cosine) + cosine;
+	m_matrix[4] =  x * y * one_minus_cosine - z * sine;
+	m_matrix[8] =  x * z * one_minus_cosine + y * sine;
+
+	m_matrix[1] =  x * y * one_minus_cosine + z * sine;
+	m_matrix[5] =  y * y * (1.0 - cosine) + cosine;
+	m_matrix[9] =  y * z * one_minus_cosine - x * sine;
+
+	m_matrix[2] =  x * z * one_minus_cosine - y * sine;
+	m_matrix[6] =  y * z * one_minus_cosine + x * sine;
+	m_matrix[10] =  z * z * (1.0 - cosine) + cosine;
+
+
+
+
+	m_matrix[3] = 0;
+	m_matrix[7] = 0;
+	m_matrix[11] = 0;
+	m_matrix[15] = 1;
+
+
+
 }
 
 void Matrix::setRotationRadians( const float *angles )
@@ -112,6 +266,11 @@ void Matrix::setRotationRadians( const float *angles )
 	m_matrix[9] = ( float )( crsp*sy-sr*cy );
 	m_matrix[10] = ( float )( cr*cp );
 }
+
+
+
+
+
 
 void Matrix::setInverseRotationRadians( const float *angles )
 {
