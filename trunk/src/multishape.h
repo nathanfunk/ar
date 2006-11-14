7c5f7d9788ecdd71d46aa9579954efe21d513614
farooq <farooq@3fcdfe01-befb-4de0-9aaa-81d4169d85b9>
2006-11-14 20:05:46 +0000

    calculate centroid for rotations etc, changed grouping to work better

--- trunk/src/multishape.h
+++ trunk/src/multishape.h
@@ -10,46 +10,77 @@ public:
 		//shapePtrs = _shapePtrs;
 		//copy(_shapePtrs.begin(), _shapePtrs.end(), shapePtrs.begin());
 		copy(_shapePtrs.begin(), _shapePtrs.end(), std::back_insert_iterator<std::vector<object *> >(shapePtrs));
+		
+
+for (int i = 0 ; i < (int) shapePtrs.size(); i++){
+		shapePtrs[i]->isSelected = 0;
+
+}
+
+		centroid.x = 0; centroid.y = 0; centroid.z = 0;
+		calcCentroid();
+
 	};
 
 object* clone()   { return new multiShape(*this); }
 
 std::vector<object *> ungroup(){
-	
- for (std::vector<object *>::iterator it = shapePtrs.begin(); it!=shapePtrs.end();) {
-	 (*it)->transform(xOff, yOff, zOff, rX, rY, rZ, sX, sY, sZ);
-	 ++it;
- }
 	return shapePtrs;
 }
+
+
+
+void calcCentroid(){
+	
+	float x, y, z;
+	x = 0; y = 0; z = 0;
+	if (shapePtrs.size() == 0) return;
+
+	for (int i = 0 ; i < (int) shapePtrs.size(); i++){
+		x+= shapePtrs[i]->tMatrix.m_matrix[12];
+		y+= shapePtrs[i]->tMatrix.m_matrix[13];
+		z+= shapePtrs[i]->tMatrix.m_matrix[14];
+	}
+
+	centroid.x = x/ shapePtrs.size();
+	centroid.y = y/ shapePtrs.size();
+	centroid.z = z/ shapePtrs.size();
+
+	std::cout<<"Centroid: "<<centroid.x<<" "<<centroid.y<<" "<<centroid.z<<std::endl;
+}
+
 
 virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 		
 		double xNew, yNew;
 			getTransformedMotion(patt_trans, but, key, x, y, xNew, yNew);
 
-
-
 		if (key == GLUT_ACTIVE_ALT){
-		if (but == GLUT_LEFT_BUTTON){
-		sX += (float)x / 25; 
-		 sY -= (float)y / 25;
-		////sZ -= (float)y / 25; 
-		}
-	
+			if (but == GLUT_LEFT_BUTTON){
+			sX += (float)x / 25; 
+			sY -= (float)y / 25;
+			////sZ -= (float)y / 25; 
+			}
 		else if (but == GLUT_MIDDLE_BUTTON){
-		 sZ -= (float)y / 25; 
-		 ///sY -= (float)y / 25;
+			sZ -= (float)y / 25; 
+			///sY -= (float)y / 25;
+			}
 		}
-		}
-	else if (key == GLUT_ACTIVE_CTRL){
-		if (but == GLUT_LEFT_BUTTON){
-		rX += x; 
+		else if (key == GLUT_ACTIVE_CTRL){
+			if (but == GLUT_LEFT_BUTTON){
+			//rX += x; 
+			//rotate each object about the centroid
+			for (int i = 0 ; i < (int) shapePtrs.size(); i++){
+				
+					shapePtrs[i]->rotate(x, centroid.x, centroid.y, centroid.z, 0, 1, 0);
+			//x+= shapePtrs[i]->xOff;
+			//y+= shapePtrs[i]->yOff;
+			//z+= shapePtrs[i]->zOff;
+			}
+			calcCentroid();
 		////rY-=y; //yOff += y;
 		}
-		else if (but == GLUT_RIGHT_BUTTON){
-		rX += x;
-		}
+
 		else if (but == GLUT_MIDDLE_BUTTON){
 		rX+=x;
 		}
@@ -61,13 +92,18 @@ virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 
 		if (but == GLUT_LEFT_BUTTON){
 		
-			xOff += xNew; zOff += yNew;
-
-
-
+			//xOff += xNew; zOff += yNew;
+	for (int i = 0 ; i < (int) shapePtrs.size(); i++){
+			shapePtrs[i]->tMatrix.translate(xNew, 0, yNew);
+		}
+			calcCentroid();
 		}
 		else if (but == GLUT_MIDDLE_BUTTON){
-		 yOff -= y;
+		 //yOff -= y;
+for (int i = 0 ; i < (int) shapePtrs.size(); i++){
+			shapePtrs[i]->tMatrix.translate(0, -y, 0);
+		}
+
 		}
 		}
 
@@ -89,20 +125,20 @@ virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 
 
 		for (int i = 0; i < (int) shapePtrs.size(); i++){
-			if (shapePtrs[i]->drawMode == NORMAL||shapePtrs[i]->drawMode == WIREFRAME){
+			if (shapePtrs[i]->drawMode == NORMAL||shapePtrs[i]->drawMode == WIREFRAME||shapePtrs[i]->drawMode == OUTLINE){
 			//push i onto namestack
-			glPushName(i);
+			//glPushName(i);
 				shapePtrs[i]->drawTopLevel(5,5,5);
-			glPopName();
+			//glPopName();
 			}
 
 		}
 		for (int i = 0; i < (int) shapePtrs.size(); i++){
 			if (shapePtrs[i]->drawMode == TRANSPARENT){
 			//push i onto namestack
-			glPushName(i);
+			//glPushName(i);
 			shapePtrs[i]->drawTopLevel(5,5,5);
-			glPopName();
+			//glPopName();
 			}
 
 
@@ -115,7 +151,7 @@ virtual void move(double patt_trans[3][4],int but, int key, int x, int y){
 	
 
 	std::vector<object *> shapePtrs;
-	
+	vertex centroid;
 	
 };
 
